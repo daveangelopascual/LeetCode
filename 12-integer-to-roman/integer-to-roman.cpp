b3741cf63f6f5b8917ea8235188e6defb8c496d7
daveangelopascual <daveangelopascual@gmail.com>
2024-12-01 11:47:26 -0500

    Time: 1 ms (79.52%) | Memory: 8.2 MB (95.69%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -4,11 +4,11 @@ public:
         string romanNumeral = "";
 
         while (num != 0) {
-            while (num / 1000 >= 1){
+            if (num / 1000 >= 1){
                 num -= 1000;
                 romanNumeral += 'M';
             }
-            while (num / 500 >= 1){
+            else if (num / 500 >= 1){
                 if (num / 900 >= 1){
                     num -= 900;
                     romanNumeral += "CM";
@@ -18,7 +18,7 @@ public:
                     romanNumeral += 'D';
                 }
             }
-            while (num / 100 >= 1){
+            else if (num / 100 >= 1){
                 if (num / 400 >= 1){
                     num -= 400;
                     romanNumeral += "CD";
@@ -28,7 +28,7 @@ public:
                     romanNumeral += "C";
                 }
             }
-            while (num / 50 >= 1){
+            else if (num / 50 >= 1){
                 if (num / 90 >= 1){
                     num -= 90;
                     romanNumeral += "XC";
@@ -38,7 +38,7 @@ public:
                     romanNumeral += 'L';
                 }
             }
-            while (num / 10 >= 1){
+            else if (num / 10 >= 1){
                 if (num / 40 >= 1){
                     num -= 40;
                     romanNumeral += "XL";
@@ -48,7 +48,7 @@ public:
                     romanNumeral += "X";
                 }
             }
-            while (num / 5 >= 1){
+            else if (num / 5 >= 1){
                 if (num / 9 >= 1){
                     num -= 9;
                     romanNumeral += "IX";
@@ -58,7 +58,7 @@ public:
                     romanNumeral += 'V';
                 }
             }
-            while (num / 1 >= 1){
+            else if (num / 1 >= 1){
                 if (num / 4 >= 1){
                     num -= 4;
                     romanNumeral += "IV";
