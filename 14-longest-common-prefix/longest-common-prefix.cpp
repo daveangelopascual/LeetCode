73510b09b6667dfa2ce4a00d07ff4cd6da77ab70
daveangelopascual <daveangelopascual@gmail.com>
2024-12-02 12:29:54 -0500

    Time: 0 ms (100.00%) | Memory: 12 MB (14.09%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -9,8 +9,8 @@ public:
             if (i.size() < smallestStr.size()) //finds smallest string size in vector
                 smallestStr = i;
         }
-        for (int i = 0; i < strs.size(); i++) { //iterates over the length vector
-            while (strs[i].find(smallestStr) != 0) { //checks if each element in vector has the same string
+        for (string str : strs) { //iterates over the length vector
+            while (str.find(smallestStr) != 0) { //checks if each element in vector starts with same string
                 smallestStr = smallestStr.substr(0, smallestStr.size() - 1); //decrease string size if false
                 if (smallestStr == "") 
                     return "";
