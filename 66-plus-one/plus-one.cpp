2f140c62f05d32bc38393d8be1fff05759e35062
daveangelopascual <daveangelopascual@gmail.com>
2024-12-04 11:59:35 -0500

    Time: 0 ms (100.00%) | Memory: 11.5 MB (5.69%) - LeetSync

---
+++ 66-plus-one/plus-one.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        if (digits[digits.size()-1] < 9)
+            digits[digits.size()-1]++;
+        else{
+            for (int i = digits.size()-1; i >= 0; i--) {
+                if (digits[i] == 9 && i != 0){
+                    digits[i] = 0;
+                }
+                else if (digits[i] == 9){
+                    digits[i] = 1;
+                    digits.push_back(0);
+                    break;
+                }
+                else {
+                    digits[i]++;
+                    break;
+                }
+            }
+        }
+        return digits;
+    }
+};
