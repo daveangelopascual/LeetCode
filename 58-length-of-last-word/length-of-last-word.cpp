0ec2c8ee6125dcc2dbd4b228031254c9e033b705
daveangelopascual <daveangelopascual@gmail.com>
2024-12-08 22:58:53 -0500

    Time: 0 ms (100.00%) | Memory: 8.9 MB (6.51%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int length = 0;
+        int i = s.length() - 1;
+        while (i >= 0 && s[i] == ' ') // Trim trailing spaces
+            i--;
+        while (i >= 0 && s[i] != ' ') { // Count the length of the last word
+            length++;
+            i--;
+    }
+        
+        return length;
+    }
+};
