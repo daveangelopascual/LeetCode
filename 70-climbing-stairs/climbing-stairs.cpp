3b0707002419c36bd233082eeaa482b9edc1e0ad
daveangelopascual <daveangelopascual@gmail.com>
2024-12-08 23:45:33 -0500

    Time: 0 ms (100.00%) | Memory: 7.8 MB (22.86%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -5,10 +5,16 @@ public:
             return 1;
         else if (n == 2)
             return 2;
+
+        // 3 steps is the sum of 1 & 2 step combos
+        // 4 steps is the sum of 2 & 3 step combos
+        // 5 steps is the sum of 3 & 4 step combos
+        // etc...
+
         int last = 2, beforeLast = 1, current;
-        for ( int i = 3; i <= n; i++) {
-            current = last + beforeLast;
-            beforeLast = last;
+        for ( int i = 3; i <= n; i++) { 
+            current = last + beforeLast; 
+            beforeLast = last; 
             last = current;
         }
         return current;
