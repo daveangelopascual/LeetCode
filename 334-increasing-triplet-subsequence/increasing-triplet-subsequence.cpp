d1bc7b3ca3972e91919450480963924b5749f095
daveangelopascual <daveangelopascual@gmail.com>
2024-12-12 14:56:00 -0500

    Time: 0 ms (100.00%) | Memory: 115.7 MB (13.47%) - LeetSync

---
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int smallest = INT_MAX, smaller = INT_MAX;
+        for (int i :nums){
+            if (i <= smallest) //replaces if a smallest number is found
+                smallest = i;
+            
+            else if (i <= smaller) //replaces if a smaller number is found
+                smaller = i;
+            
+            else //if smallest and smaller number was found
+                return true;
+        }
+        return false;
+    }
+};
