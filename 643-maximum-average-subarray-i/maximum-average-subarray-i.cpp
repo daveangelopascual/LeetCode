4924aaa13eea6385bd55bebe192680ecc128dc62
daveangelopascual <daveangelopascual@gmail.com>
2024-12-13 14:38:23 -0500

    Time: 0 ms (100.00%) | Memory: 113.6 MB (25.90%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -3,16 +3,16 @@ public:
     double findMaxAverage(vector<int>& nums, int k) {
         double sum = 0.0;
 
-    for (int i = 0; i < k; i++) { // calculate the initial window sum
-        sum += nums[i];
-    }
+        for (int i = 0; i < k; i++) { // calculate the initial window sum
+            sum += nums[i];
+        }
 
-    double avg = sum / k;
+        double avg = sum / k;
 
-    for (int i = k; i < nums.size(); i++) { // slide the window across the array
-        sum += nums[i] - nums[i - k]; // add the new element, remove the old one
-        avg = max(avg, sum / k);
-    }
-    return avg;
+        for (int i = k; i < nums.size(); i++) { // slide the window across the array
+            sum += nums[i] - nums[i - k]; // add the new element, remove the old one
+            avg = max(avg, sum / k);
+        }
+        return avg;
     }
 };
